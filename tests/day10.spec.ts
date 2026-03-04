71f93e299ba1da702e3bfc71a5396e1a8f2bdf65
AKSHARA <akshara.ashokan11@gmail.com>
2026-03-04 17:57:03 +0530

    Day 10 User Adding

---
+++ tests/day10.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+// 1. Import your JSON data
+import testData from '../data/users.json';
+
+test.describe('Day 14: Data-Driven Login Tests', () => {
+
+    // 2. Loop through each object in the JSON array
+    for (const user of testData) {
+        
+        test(`Login Attempt for: ${user.desc}`, async ({ page }) => {
+            await page.goto('https://the-internet.herokuapp.com/login');
+            
+            await page.locator('#username').fill(user.username);
+            await page.locator('#password').fill(user.password);
+            await page.getByRole('button', { name: 'Login' }).click();
+
+            if (user.desc === 'Valid User') {
+                await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
+            } else {
+                await expect(page.locator('#flash')).toContainText('is invalid!');
+            }
+        });
+    }
+});
